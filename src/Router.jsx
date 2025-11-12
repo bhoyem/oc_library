c9588d7d9de84e0f2f0b5877edb1ebe28a1ab302
Bryan Hoyem <bjhoyem@gmail.com>
2025-11-12 17:38:24 -0600

    Re-created with Vite.

--- src/Router.jsx
+++
@@ -1,7 +0,0 @@
-import { createBrowserRouter, RouterProvider, Navigate } from "react-router";
-
-import LandingPageLayout form "/ui/LandingPageLayout";
-
-const router = createBrowserRouter([
-    
-])
